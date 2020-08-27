bd82cc93ee1a27cf2bb9a42e614b69f2ee818af7
annisaaadindra <annisaaadindra@gmail.com>
2020-08-27 23:10:43 +0700

    fetching data

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('tes');
+var topQuotes = [];
+fetch("https://quotes-api.archv.id/api/quotes/v1/latest")
+    // return to response.json
+    .then(response => response.json())
+    .then(resJson => {
+        resJson.content.map((result, i) => {
+            if (i < 3) {
+               printQuotes(result, i);
+            }
+        })
+    });
+
+function printQuotes(content, index)
+{
+    document.getElementById(`quote-${index}`).innerHTML = content.content;
+    document.getElementById(`author-${index}`).innerHTML = `- ${content.author}`;
+}
